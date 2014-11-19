bfb7591427f22fea5d48420397641a427650d308
John Bender <john.m.bender@gmail.com>
2014-11-19 01:15:27 -0500

    get the front end basics wired up

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,33 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var fs = require( 'fs' );
+
+// fake data to start
+var data = {
+  todos: [
+    {
+      title: "foo",
+      completed: true
+    },
+
+    {
+      title: "bar",
+      completed: false
+    },
+
+    {
+      title: "baz",
+      completed: false
+    }
+  ]
+};
+
+
+var templates = [];
+
+// get the templates for inclusion in the document
+fs.readdirSync("views/client/").forEach(function( file ) {
+  templates.push({
+    name: file.toString().replace(/\.mustache/, ""),
+    markup: fs.readFileSync("views/client/" + file)
+  });
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var data = {
-    todos: [
-      {
-        title: "foo",
-        completed: true
-      },
-
-      {
-        title: "bar",
-        completed: false
-      },
-
-      {
-        title: "baz",
-        completed: false
-      }
-    ]
-  };
-
   var remaining;
 
   res.render('index', {
+    templates: templates,
+
     todos: data.todos,
 
-    // todo base on values
+    // TODO base on values
     completed: 1,
     remaining: remaining = 2,
     remainingPlural: remaining > 1,
