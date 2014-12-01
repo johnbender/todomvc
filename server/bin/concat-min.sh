f289ebcc976624ad9457fb34ec626962f8629a1b
John Bender <john.m.bender@gmail.com>
2014-12-01 01:57:00 -0500

    switched to project specific js names for device timing results

--- server/bin/concat-min.sh
+++ server/bin/concat-min.sh
@@ -1,23 +1,32 @@
 index_files=$( find -L $1  -maxdepth 2 -name "index.html" )
 
+
+echo "Project index files to get js from: "
 echo $index_files
+echo
 
 for file in $index_files; do
-  sub_dir=$( echo $file | sed 's/index.html//' )
+  sub_dir=$( echo $file | sed 's/\/index.html//' )
+  project=$( echo $file | sed 's/.*\/\([a-z_]*\)\/index.html/\1/' )
 
-  echo $file
+  echo "Working on:  $file"
+  echo "Project:     $project"
 
-  echo "" > "$sub_dir/all.js"
+  all_file="$sub_dir/$project.all.js"
+  min_file="$sub_dir/$project.all.min.js"
+
+  echo "" > "$sub_dir/$project.all.js"
 
   for js in $( cat $file | grep "script src" | sed 's/.*src="\(.*\)".*/\1/' ); do
     if echo "$js" | grep "all\." > /dev/null; then
-      echo "skipping $js"
+      echo "Skipping:    *.all.js"
       continue;
     fi
 
-    cat "$sub_dir$js" >> "$sub_dir/all.js"
+    cat "$sub_dir/$js" >> "$all_file"
   done
 
-  echo "minifying ..."
-  node node_modules/.bin/minify --output "$sub_dir/all.min.js" "$sub_dir/all.js"
+  echo "Minifying:   $all_file ..."
+  node node_modules/.bin/minify --output $min_file $all_file
+  echo
 done
